62ae44a8d93a70259a76639fad947bedb8da5a53
Jean Cunha <jean.cunha13@hotmail.com>
2023-10-20 15:42:03 -0300

    try commit

--- src/main/java/rest/controller/ClienteController.java
+++ src/main/java/rest/controller/ClienteController.java
@@ -1,8 +1,10 @@
 package rest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
 
