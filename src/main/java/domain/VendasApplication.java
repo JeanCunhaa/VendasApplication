cab576766405687b35531834a5ab1e196fd53cfe
Jean Cunha <jean.cunha13@hotmail.com>
2023-10-26 16:29:24 -0300

    Finalizando

--- src/main/java/domain/VendasApplication.java
+++ src/main/java/domain/VendasApplication.java
@@ -1,32 +1,24 @@
 package domain;
 
 import domain.entity.Cliente;
+import domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import domain.repository.Clientes;
-
-import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
 
-    @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes){
-        return args -> {
-            System.out.println("Salvando clientes");
+//    @Bean
+//    public CommandLineRunner commandLineRunner (@Autowired Clientes clientes) {
+//        return args -> {
+//          Cliente c = new Cliente(null, "Jean");
+//          clientes.save(c);
+//        };
+//    }
 
-            clientes.save(new Cliente("Jean"));
-            clientes.save(new Cliente("Kenji Yasuda"));
-
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-
-    };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
